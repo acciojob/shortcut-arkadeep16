54a0cf9273123b0d986218735d1e4cb305d973a9
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-04-17 02:30:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
+	return s1.slice(0,1) + s2.slice(0,1);
+
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
